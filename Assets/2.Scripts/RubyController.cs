ab11b4ecc6eba6cc0d35bc54ab22cea9b8e7f175
zinine73 <zinine@naver.com>
2025-02-04 15:49:18 +0900

    move

--- Assets/2.Scripts/RubyController.cs
+++ Assets/2.Scripts/RubyController.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float moveSpeed = 4.0f; 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        // GetAxisLaw를 사용하면 -1,1값이 넘어온다
+        //float vertical = Input.GetAxisRaw("Vertical");
+        //Debug.Log($"H:{horizontal}");
+        //Debug.Log($"V:{vertical}");
         Vector2 position = transform.position;
-        position.x += 0.1f;
+        position.x += moveSpeed * horizontal * Time.deltaTime;
+        position.y += moveSpeed * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
